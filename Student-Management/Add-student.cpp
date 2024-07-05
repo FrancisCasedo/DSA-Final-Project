bce8b31416a933a47b7ffbe1b80b6d54a4eb0f9e
FrancisCasedo <202310741@fit.edu.ph>
2024-07-05 20:06:47 +0800

    Add-Student added but have not been checked for bugs

--- Student-Management/Add-student.cpp
+++ Student-Management/Add-student.cpp
@@ -32,9 +32,14 @@ void Addstud(){
     string Year;
     string yearInput;
     string birthday;
-    string Degree;
     string address;
-    char Gender;
+    string DegreeList[5] = {"Bachelor of Science in Computer Science", "Bachelor of Science in Information Technology", "Bachelor of Science in Computer Engineering", "Bachelor of Science in Information Systems", "Bachelor of Science in Information Technology with Specialization in Cybersecurity"};
+    string degreeInput;
+    string degree;
+    string yearList[4] ={"1st year","2nd Year","3rd Year","4th year"};
+    string yearLevel;
+    string yearLevelInput;
+    string Gender;
 
     do {
         cout << "Student Name: ";
@@ -46,6 +51,7 @@ void Addstud(){
             if (!isdigit(c)) {
                 validname = false;
                 cout << "Please enter a valid name"<< endl;
+                system("PAUSE");
                 break;
             }else{
                 Name = NameInput;
@@ -54,6 +60,17 @@ void Addstud(){
 
     } while (true);
 
+    do {
+        cout << "Gender (M/F): ";
+        getline(cin,Gender);
+        cin.ignore();
+        if (Gender != "M" && Gender != "m" && Gender != "F" && Gender != "f") {
+            cout << "Input is not included in the options" << endl;
+            system("PAUSE");
+        }
+    } while (Gender != "M" && Gender != "m" && Gender != "F" && Gender != "f");
+
+//Inputting the student's birthday
     do {
         cout << "Month: ";
         cin >> monthInput;
@@ -64,6 +81,7 @@ void Addstud(){
             if (!isdigit(c)) {
                 validmonth = false;
                 cout << "Please enter a valid month (01 - 12)." << endl;
+                system("PAUSE");
                 break;
             }
         }
@@ -75,6 +93,7 @@ void Addstud(){
                 break;
             } else {
                 cout << "Please enter a valid month (01 - 12)." << endl;
+                system("PAUSE");
             }
         }
     } while (true);
@@ -89,6 +108,7 @@ void Addstud(){
             if (!isdigit(b)) {
                 validday = false;
                 cout << "Please enter a valid day (01 - 31)." << endl;
+                system("PAUSE");
                 break;
             }
         }
@@ -100,6 +120,7 @@ void Addstud(){
                 break;
             } else {
                 cout << "Please enter a valid day (01 - 31)." << endl;
+                system("PAUSE");
             }
         }
     } while (true);
@@ -115,6 +136,7 @@ void Addstud(){
                 validyear = false;
                 cout << "Please enter a valid year" << endl;
                 break;
+                system("PAUSE");
             }
         }
 
@@ -128,7 +150,53 @@ void Addstud(){
             }
         }
     } while (true);
-
     birthday = Day + " " + Month + " " + Year;
 
+//Inputting the student's Degree
+do{
+    cout << "Degree: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "[" << i + 1 << DegreeList[i] << "]" <<  endl;
+    }
+    cout << ": ";
+    getline(cin,degreeInput);
+
+
+    if (degreeInput.length() == 1){
+        if(degreeInput == "1"|| degreeInput == "2"|| degreeInput == "3"|| degreeInput == "4" || degreeInput == "5"){
+            degree = DegreeList[stoi(degreeInput) - 1];
+        }
+    }else{
+    degreeInput = " ";
+    cout << "Invalid Input, input a number from 1 - 5" << endl;
+    system("PAUSE");
+     }
+     
+    }while(degreeInput == " ");
+
+//Inputting the student's year level
+    do{
+    cout << "Year Level [1-4]" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "[" << i + 1 << yearList[i] << "]" <<  endl;
+    }
+    cout << ": ";
+    cin >> yearLevelInput;
+
+    if (yearLevelInput.length() == 1 ){
+        if( yearLevelInput == "1"|| yearLevelInput == "2"|| yearLevelInput == "3"|| yearLevelInput == "4" || yearLevelInput == "5" ){
+            yearLevel = yearList[stoi(yearInput) - 1];
+        }
+    }else{
+        yearLevelInput = " ";
+        cout << "Invalid Input, input a number from 1 - 4" << endl;
+        system("PAUSE");
+    }
+    }while (yearLevelInput == " ");
+
+    StudentList student(Name, yearLevel, ID, birthday, address, degree, Gender[0]);
+
+    
 }
